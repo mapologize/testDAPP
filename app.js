64754ab357838e9f89e760c91ccffe0e78bc2742
Penzer <dedonoiizunfinity@gmail.com>
2023-12-05 05:54:21 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const port = 1234;
+const app = express();
+
+app.use(cors());
+
+app.get('/',(req, res, next) => {
+    res.json({ msg: "cors api get done!"});
+});
+
+app.listen(port,() => {
+    console.log('cors webserver listening port : '+port);
+});
+
+module.exports = app;
